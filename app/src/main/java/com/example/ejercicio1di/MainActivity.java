f071a2343401f5d1822aa4c6bd9bc33ea5f0972e
MorenoSegoviaSamuel <morenosegoviasamuel@gmail.com>
2023-10-27 19:37:50 +0200

    ToolBar añadida

--- app/src/main/java/com/example/ejercicio1di/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1di/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
         newPlayer= findViewById(R.id.button2);
         newPlayer.setOnClickListener(this::lanzarNewPlayer);
+
     }
 
     public void lanzarNewPlayer(View view) {
         Intent intent = new Intent();
         intent.setClass(this,NewPlayer.class);
         startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelection(MenuItem item){
+      int id = item.getItemId();
+      if (id == R.id.action_buscar){
+          Toast.makeText(this,"Búsqueda",Toast.LENGTH_LONG).show();
+          return true;
+      }
+      return super.onOptionsItemSelected(item);
     }
 }
 
